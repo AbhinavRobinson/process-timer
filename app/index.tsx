46625c5c0493ede47f0ef290c5cd269c69d97c55
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-18 21:53:13 +0530

    (feat): stylings and layout ✨

--- app/index.tsx
+++ app/index.tsx
@@ -12,26 +12,85 @@ class App extends React.Component<{}, { static: string }> {
 	}
 
 	render() {
-		const timeSpent = [0, 0, 0, 0]
+		const timeSpent = [100, 75, 50, 25]
 
 		return (
-			<div className='container flex flex-column'>
-				<ul className='app-list flex flex-column'>
-					<li>
-						<span className='app-item'>{timeSpent[0]}</span>%
-					</li>
-					<li>
-						<span className='app-item'>{timeSpent[1]}</span>%
-					</li>
-					<li>
-						<span className='app-item'>{timeSpent[2]}</span>%
-					</li>
-					<li>
-						<span className='app-item'>{timeSpent[3]}</span>%
-					</li>
-				</ul>
-				<button className='play-button bg-dark'>▶</button>
+			<div style={{
+				maxWidth: '50px',
+				background:'#ccc',
+				padding:'10px',
+				borderRadius:'15px',
+				fontFamily:'monospace'
+			}}>
+				<div className='container' style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'center'
+				}}>
+					<ul className='app-list' style={{
+						listStyle: 'none',
+						padding: '0 0 5px 0',
+						margin: 0
+					}}>
+						<li style={{
+							color: 'white',
+							background: '#333',
+							maxWidth: '25px',
+							minHeight: '25px',
+							borderRadius: '25px',
+							padding: '5px',
+							textAlign: 'center',
+							margin: '5px 0'
+						}}>
+							<span className='app-item'>{timeSpent[0]}</span>
+						</li>
+						<li style={{
+							color: 'white',
+							background: '#333',
+							maxWidth: '25px',
+							minHeight: '25px',
+							borderRadius: '25px',
+							padding: '5px',
+							textAlign: 'center',
+							margin: '5px 0'
+						}}>
+							<span className='app-item'>{timeSpent[1]}</span>
+						</li>
+						<li style={{
+							color: 'white',
+							background: '#333',
+							maxWidth: '25px',
+							minHeight: '25px',
+							borderRadius: '25px',
+							padding: '5px',
+							textAlign: 'center',
+							margin: '5px 0'
+						}}>
+							<span className='app-item'>{timeSpent[2]}</span>
+						</li>
+						<li style={{
+							color: 'white',
+							background: '#333',
+							maxWidth: '25px',
+							minHeight: '25px',
+							borderRadius: '25px',
+							padding: '5px',
+							textAlign: 'center',
+							margin: '5px 0'
+						}}>
+							<span className='app-item'>{timeSpent[3]}</span>
+						</li>
+					</ul>
+					<button className='play-button' style={{
+						maxWidth: '25px',
+						minHeight: '25px',
+						background: '#ccc',
+						border:'2px solid #333',
+						borderRadius: '25px'
+					}}>▶</button>
+				</div>
 			</div>
+
 		)
 	}
 }
