06eb5dbe2540da8aea320e8cfe8630fd9cc1afdf
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-12-19 19:32:12 +0530

    feat: get app name

--- static/getwindow.py
+++ static/getwindow.py
@@ -1,15 +1,20 @@
-from ctypes import wintypes, windll, create_unicode_buffer
+from ctypes import wintypes, windll, create_unicode_buffer,byref
+import psutil
 
 def getForegroundWindowTitle():
     hWnd = windll.user32.GetForegroundWindow()
     length = windll.user32.GetWindowTextLengthW(hWnd)
     buf = create_unicode_buffer(length + 1)
     windll.user32.GetWindowTextW(hWnd, buf, length + 1)
+    pid = wintypes.DWORD()
     
+    active_window = windll.user32.GetWindowThreadProcessId(hWnd,byref(pid)) 
+    # print(pid)
     if buf.value:
-        return buf.value
+        return buf.value,pid
     else:
         return None
 
 # while(True):
-print(getForegroundWindowTitle(),end="")
+title,pid=getForegroundWindowTitle()
+print({'title':title,'app':pid.value,'pid':pid.value})
