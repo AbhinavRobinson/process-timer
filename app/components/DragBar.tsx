c4a205bc8e750f4371e4fa3b5b14f94a28267b62
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-27 17:54:31 +0530

    (feat): added placeholder close button

--- app/components/DragBar.tsx
+++ app/components/DragBar.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 
+/**
+ * Font Awesome Imports
+ */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
 const DragBar = (): JSX.Element => (
-	<div
-		className='draggable xxs text-center'
-		style={{
-			minWidth: '100%',
-			minHeight: '30px',
-			margin: 0,
-			backgroundColor: 'black',
-		}}
-	></div>
+	<div className="topbar grid grid-5" style={{
+		minWidth: '100%',
+		minHeight: '30px',
+		margin: 0,
+		backgroundColor: 'black',
+		alignItems: 'center'
+	}}>
+		<div className='draggable xxs text-center' style={{height: '100%', width:'80%', gridColumn:'1/5'}}></div>
+		<div className="close-button text-center" style={{backgroundColor:'orange',color: 'white', placeSelf: "center stretch", height:'100%'}}><FontAwesomeIcon icon={faTimes} /></div>
+	</div>
 )
 
 export default DragBar
