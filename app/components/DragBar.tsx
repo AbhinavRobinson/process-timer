1d3bd06b823e651db8700a047f69faff1a041a88
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-27 20:03:32 +0530

    (feat): fixed dragbar to top

--- app/components/DragBar.tsx
+++ app/components/DragBar.tsx
@@ -8,40 +8,47 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const DragBar = (): JSX.Element => (
-	<div
-		className='topbar grid draggable'
-		style={{
-			minWidth: '100%',
-			margin: 0,
-			backgroundColor: 'black',
-			gridTemplateColumns: '9fr 1fr',
-			gap: 0
-		}}
-	>
+	<div className="container">
 		<div
-			className='text-center'
+			className='topbar grid draggable'
 			style={{
-				height: '100%',
-				color: 'white',
-				padding: '0 10px',
-				placeSelf: 'start center'
-			}}
-		>Connect with Buddy</div>
-		<div
-			className='close-button rg text-center'
-			style={{
-				backgroundColor: 'orange',
-				color: 'white',
-				placeSelf: 'center stretch',
-				height: '100%',
-			}}
-			onClick={() => {
-				CloseHandler("question",'Do you want to close this window?')
+				minWidth: '100%',
+				margin: 0,
+				backgroundColor: 'black',
+				gridTemplateColumns: '9fr 1fr',
+				gap: 0,
+				position: 'fixed',
+				top: '0px',
+				maxHeight: '25px'
 			}}
 		>
-			<FontAwesomeIcon icon={faTimes} />
+			<div
+				className='text-center'
+				style={{
+					height: '100%',
+					color: 'white',
+					padding: '0 10px',
+					placeSelf: 'start center'
+				}}
+			>Connect with Buddy</div>
+			<div
+				className='close-button rg text-center'
+				style={{
+					backgroundColor: 'orange',
+					color: 'white',
+					placeSelf: 'center stretch',
+					height: '100%',
+				}}
+				onClick={() => {
+					CloseHandler("question", 'Do you want to close this window?')
+				}}
+			>
+				<FontAwesomeIcon icon={faTimes} />
+			</div>
 		</div>
+		<div className="spacer" style={{height:'25px'}}></div>
 	</div>
+
 )
 
 export default DragBar
