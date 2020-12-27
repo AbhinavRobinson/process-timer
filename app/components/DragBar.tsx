7cbb4ea155838906d8a1247544a60ca196d26ad4
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-12-27 17:56:37 +0530

    (feat): close button in center

--- app/components/DragBar.tsx
+++ app/components/DragBar.tsx
@@ -9,13 +9,12 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'
 const DragBar = (): JSX.Element => (
 	<div className="topbar grid grid-5" style={{
 		minWidth: '100%',
-		minHeight: '30px',
 		margin: 0,
 		backgroundColor: 'black',
 		alignItems: 'center'
 	}}>
 		<div className='draggable xxs text-center' style={{height: '100%', width:'80%', gridColumn:'1/5'}}></div>
-		<div className="close-button text-center" style={{backgroundColor:'orange',color: 'white', placeSelf: "center stretch", height:'100%'}}><FontAwesomeIcon icon={faTimes} /></div>
+		<div className="close-button rg text-center" style={{backgroundColor:'orange',color: 'white', placeSelf: "center stretch", height:'100%'}}><FontAwesomeIcon icon={faTimes} /></div>
 	</div>
 )
 
