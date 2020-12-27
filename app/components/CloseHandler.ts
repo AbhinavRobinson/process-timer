198f76b8b3ee6735899a9b8daa30a1384aa5a3c7
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-12-27 19:06:16 +0530

    fix: rename

--- app/components/CloseHandler.ts
+++ app/components/CloseHandler.ts
@@ -1,17 +1,17 @@
-import { remote } from 'electron';
+import { remote } from 'electron'
 
 export function CloseHandler(textmessage: string, callback?: VoidFunction) {
 	remote.dialog
 		.showMessageBox(null, {
 			buttons: ['Sure.', 'No.'],
-			message: textmessage
+			message: textmessage,
 		})
 		.then((data) => {
 			if (data.response === 0) {
-				window.close();
+				window.close()
 			}
 			if (data.response) {
-				callback();
+				callback()
 			}
-		});
+		})
 }
