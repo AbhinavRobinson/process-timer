7a3a68ddd143869388dca94da2d3126e6fe48a83
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-04 01:21:20 +0530

    (feat): add Agora boilerplate

--- app/components/Video.ts
+++ app/components/Video.ts
@@ -1,6 +1,15 @@
 import '../../token.json'
 
+import AgoraRtcEngine from 'agora-electron-sdk'
+
 export default function Video(height?: number, width?: number) {
+	// RtcEngine.initialize(appid)
+	// RtcEngine.setupLocalVideo(element)
+	// RtcEngine.enableVideo()
+	// RtcEngine.joinChannel(token, channel, info, uid)
+	// RtcEngine.setupRemoveVideo(uid, view, info, channel)
+	// RtcEngine.leaveChannel()
+
 	console.log(height, width)
 	return 'video stream'
 }
