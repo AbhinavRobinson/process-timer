e9f55a4fc1f40821335a70ae6ea5810d2aa7de5b
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-09 19:32:06 +0530

    (fix): leave button not working

--- app/hooks/useAgora.tsx
+++ app/hooks/useAgora.tsx
@@ -1,98 +1,110 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import AgoraRTC, {
-  IAgoraRTCClient, IAgoraRTCRemoteUser, MicrophoneAudioTrackInitConfig, CameraVideoTrackInitConfig, IMicrophoneAudioTrack, ICameraVideoTrack, ILocalVideoTrack, ILocalAudioTrack
-} from 'agora-rtc-sdk-ng';
+	IAgoraRTCClient,
+	IAgoraRTCRemoteUser,
+	MicrophoneAudioTrackInitConfig,
+	CameraVideoTrackInitConfig,
+	IMicrophoneAudioTrack,
+	ICameraVideoTrack,
+	ILocalVideoTrack,
+	ILocalAudioTrack,
+} from 'agora-rtc-sdk-ng'
 
-export default function useAgora(client: IAgoraRTCClient | undefined)
-  : {
-    localAudioTrack: ILocalAudioTrack | undefined,
-    localVideoTrack: ILocalVideoTrack | undefined,
-    joinState: boolean,
-    leave: Function,
-    join: Function,
-    remoteUsers: IAgoraRTCRemoteUser[],
-  } {
-  const [localVideoTrack, setLocalVideoTrack] = useState<ILocalVideoTrack | undefined>(undefined);
-  const [localAudioTrack, setLocalAudioTrack] = useState<ILocalAudioTrack | undefined>(undefined);
+export default function useAgora(
+	client: IAgoraRTCClient | undefined
+): {
+	localAudioTrack: ILocalAudioTrack | undefined
+	localVideoTrack: ILocalVideoTrack | undefined
+	joinState: boolean
+	leave: Function
+	join: Function
+	remoteUsers: IAgoraRTCRemoteUser[]
+} {
+	const [localVideoTrack, setLocalVideoTrack] = useState<ILocalVideoTrack | undefined>(undefined)
+	const [localAudioTrack, setLocalAudioTrack] = useState<ILocalAudioTrack | undefined>(undefined)
 
-  const [joinState, setJoinState] = useState(false);
+	const [joinState, setJoinState] = useState(false)
 
-  const [remoteUsers, setRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([]);
+	const [remoteUsers, setRemoteUsers] = useState<IAgoraRTCRemoteUser[]>([])
 
-  async function createLocalTracks(audioConfig?: MicrophoneAudioTrackInitConfig, videoConfig?: CameraVideoTrackInitConfig)
-    : Promise<[IMicrophoneAudioTrack, ICameraVideoTrack]> {
-    const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks(audioConfig, videoConfig);
-    setLocalAudioTrack(microphoneTrack);
-    setLocalVideoTrack(cameraTrack);
-    return [microphoneTrack, cameraTrack];
-  }
+	async function createLocalTracks(
+		audioConfig?: MicrophoneAudioTrackInitConfig,
+		videoConfig?: CameraVideoTrackInitConfig
+	): Promise<[IMicrophoneAudioTrack, ICameraVideoTrack]> {
+		const [microphoneTrack, cameraTrack] = await AgoraRTC.createMicrophoneAndCameraTracks(audioConfig, videoConfig)
+		setLocalAudioTrack(microphoneTrack)
+		setLocalVideoTrack(cameraTrack)
+		return [microphoneTrack, cameraTrack]
+	}
 
-  async function join(appid: string, channel: string, token?: string, uid?: string | number | null) {
-    if (!client) return;
-    const [microphoneTrack, cameraTrack] = await createLocalTracks();
+	async function join(appid: string, channel: string, token?: string, uid?: string | number | null) {
+		if (!client) return
 
-    await client.join(appid, channel, token || null);
-    await client.publish([microphoneTrack, cameraTrack]);
+		// still sets state true even if mic or camera not connected
+		setJoinState(true)
 
-    (window as any).client = client;
-    (window as any).videoTrack = cameraTrack;
+		const [microphoneTrack, cameraTrack] = await createLocalTracks()
 
-    setJoinState(true);
-  }
+		await client.join(appid, channel, token || null)
+		await client.publish([microphoneTrack, cameraTrack])
+		;(window as any).client = client
+		;(window as any).videoTrack = cameraTrack
+	}
 
-  async function leave() {
-    if (localAudioTrack) {
-      localAudioTrack.stop();
-      localAudioTrack.close();
-    }
-    if (localVideoTrack) {
-      localVideoTrack.stop();
-      localVideoTrack.close();
-    }
-    setRemoteUsers([]);
-    setJoinState(false);
-    await client?.leave();
-  }
+	async function leave() {
+		if (localAudioTrack) {
+			localAudioTrack.stop()
+			localAudioTrack.close()
+		}
+		if (localVideoTrack) {
+			localVideoTrack.stop()
+			localVideoTrack.close()
+		}
+		setRemoteUsers([])
+		setJoinState(false)
+		await client?.leave()
+	}
 
-  useEffect(() => {
-    if (!client) { return null; }
-    else {
-      setRemoteUsers(client.remoteUsers);
+	useEffect(() => {
+		if (!client) {
+			return
+		} else {
+			setRemoteUsers(client.remoteUsers)
 
-      const handleUserPublished = async (user: IAgoraRTCRemoteUser, mediaType: 'audio' | 'video') => {
-        await client.subscribe(user, mediaType);
-        // toggle rerender while state of remoteUsers changed.
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
-      const handleUserUnpublished = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
-      const handleUserJoined = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
-      const handleUserLeft = (user: IAgoraRTCRemoteUser) => {
-        setRemoteUsers(remoteUsers => Array.from(client.remoteUsers));
-      }
-      client.on('user-published', handleUserPublished);
-      client.on('user-unpublished', handleUserUnpublished);
-      client.on('user-joined', handleUserJoined);
-      client.on('user-left', handleUserLeft);
+			const handleUserPublished = async (user: IAgoraRTCRemoteUser, mediaType: 'audio' | 'video') => {
+				await client.subscribe(user, mediaType)
+				// toggle rerender while state of remoteUsers changed.
+				setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers))
+			}
+			const handleUserUnpublished = (user: IAgoraRTCRemoteUser) => {
+				setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers))
+			}
+			const handleUserJoined = (user: IAgoraRTCRemoteUser) => {
+				setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers))
+			}
+			const handleUserLeft = (user: IAgoraRTCRemoteUser) => {
+				setRemoteUsers((remoteUsers) => Array.from(client.remoteUsers))
+			}
+			client.on('user-published', handleUserPublished)
+			client.on('user-unpublished', handleUserUnpublished)
+			client.on('user-joined', handleUserJoined)
+			client.on('user-left', handleUserLeft)
 
-      return () => {
-        client.off('user-published', handleUserPublished);
-        client.off('user-unpublished', handleUserUnpublished);
-        client.off('user-joined', handleUserJoined);
-        client.off('user-left', handleUserLeft);
-      };
-    }
-  }, [client]);
+			return () => {
+				client.off('user-published', handleUserPublished)
+				client.off('user-unpublished', handleUserUnpublished)
+				client.off('user-joined', handleUserJoined)
+				client.off('user-left', handleUserLeft)
+			}
+		}
+	}, [client])
 
-  return {
-    localAudioTrack,
-    localVideoTrack,
-    joinState,
-    leave,
-    join,
-    remoteUsers,
-  };
+	return {
+		localAudioTrack,
+		localVideoTrack,
+		joinState,
+		leave,
+		join,
+		remoteUsers,
+	}
 }
