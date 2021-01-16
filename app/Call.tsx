70f0e2faf4e628183a36b7ca72d9ae9f85a70fd8
Saksham Puri <sakshampuri1999@gmail.com>
2021-01-17 01:31:34 +0530

    Added amplify api here to get all info to connect agora

--- app/Call.tsx
+++ app/Call.tsx
@@ -4,97 +4,55 @@ import useAgora from './hooks/useAgora'
 import MediaPlayer from './components/MediaPlayer'
 import './Call.css'
 
-import Token from '../token.json'
-
 import { API } from 'aws-amplify'
+
+interface agoraState {
+	appID: string
+	token: string
+	channel: string
+}
 
 const client = AgoraRTC.createClient({ codec: 'h264', mode: 'rtc' })
 
-async function testCallApi() {
-	const res = await API.post('mainApi', '/agora/token', {})
-	return res
+async function getAgoraToken(changeLoading: React.Dispatch<React.SetStateAction<boolean>>) {
+	const { appID } = await API.get('mainApi', '/agora/appId', {})
+	const { token, channel } = await API.post('mainApi', '/agora/token', {})
+	changeLoading(false)
+	return { appID, token, channel }
 }
 
 function Call() {
-	const [appid, setAppid] = useState('')
-	// USE DYNAMIC TOKEN INSTEAD
-	const [token, setToken] = useState('')
-	// const [token] = useState('')
-	const [channel, setChannel] = useState('')
-	const { localAudioTrack, localVideoTrack, leave, join, joinState, remoteUsers } = useAgora(client)
-	const [test, changeTest] = useState({})
-	localAudioTrack
-	// GET APP ID << TEST APP ID
+	const [agoraConfig, changeAgoraConfig] = useState<agoraState | null>(null)
+	const { localVideoTrack, leave, join, joinState, remoteUsers } = useAgora(client)
+	const [loading, changeLoading] = useState<boolean>(true)
+
 	useEffect(() => {
-		testCallApi()
-			.then((res) => changeTest(res))
-			.catch(console.error)
-		setAppid(Token.Agora.APP_ID)
-		setToken(Token.Agora.TOKEN)
+		getAgoraToken(changeLoading).then(changeAgoraConfig).catch(console.error)
 	}, [])
+
+	useEffect(() => {
+		//!loading && join(agoraConfig.appID, agoraConfig.channel, agoraConfig.token)
+		if (!loading && agoraConfig) join(agoraConfig.appID, agoraConfig.channel, agoraConfig.token)
+	}, [loading, agoraConfig])
 
 	// USE DYNAMIC TOKEN INSTEAD
 	// setToken(Token.Agora.Token)
 
 	return (
 		<div className='call'>
-			<form className='call-form'>
-				{/* <label>
-					AppID:
-					<input
-						type='text'
-						name='appid'
-						onChange={(event) => {
-							setAppid(event.target.value)
-						}}
-					/>
-				</label> */}
-				{/* <label>
-					Token(Optional):
-					<input
-						type='text'
-						name='token'
-						onChange={(event) => {
-							setToken(event.target.value)
-						}}
-					/>
-				</label> */}
-				<p>{JSON.stringify(test)}</p>
-				<label>
-					Channel:
-					<input
-						type='text'
-						name='channel'
-						onChange={(event) => {
-							setChannel(event.target.value)
-						}}
-					/>
-				</label>
-				<div className='button-group'>
-					<button
-						id='join'
-						type='button'
-						className='btn btn-primary btn-sm'
-						disabled={joinState}
-						onClick={() => {
-							join(appid, channel, token)
-						}}
-					>
-						Join
-					</button>
-					<button
-						id='leave'
-						type='button'
-						className='btn btn-primary btn-sm'
-						disabled={!joinState}
-						onClick={() => {
-							leave()
-						}}
-					>
-						Leave
-					</button>
-				</div>
-			</form>
+			<div className='button-group'>
+				<button
+					id='leave'
+					type='button'
+					className='btn btn-primary btn-sm'
+					disabled={!joinState}
+					onClick={() => {
+						leave()
+					}}
+				>
+					Leave
+				</button>
+			</div>
 			<div className='player-container'>
 				<div className='local-player-wrapper'>
 					<p className='local-player-text'>
