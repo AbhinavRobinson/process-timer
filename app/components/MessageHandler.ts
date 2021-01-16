daba071cea5417a441e6671b02950acd21011c6b
Saksham Puri <sakshampuri1999@gmail.com>
2021-01-16 18:10:57 +0530

    Message Handler fixed

--- app/components/MessageHandler.ts
+++ app/components/MessageHandler.ts
@@ -9,29 +9,20 @@ import { remote } from 'electron'
  * @returns [0,1] -> {yes, no} responce id
  */
 export default async function MessageHandler(textMessage: string, callback?: VoidFunction) {
-	remote.dialog
+	return remote.dialog
 		.showMessageBox(null, {
 			message: textMessage,
 			buttons: ['Yes.', 'No!'],
 		})
-		.then(
-			(data) => {
-				try {
-					callback()
-				} catch (e) {
-					var error = e
-				} finally {
-					return data.response
-				}
-			},
-			(data) => {
-				try {
-					callback()
-				} catch (e) {
-					var error = e
-				} finally {
-					return data.response
-				}
+		.then(({ response }) => {
+			try {
+				callback()
+			} catch (e) {
+				var error = e
+				console.error(error)
+			} finally {
+				return response
 			}
-		)
+		})
+		.catch(console.error)
 }
