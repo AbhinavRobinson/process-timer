549421aaca0a402c5adeea22848cc125fd2b1ed0
Saksham Puri <sakshampuri1999@gmail.com>
2021-01-17 17:09:32 +0530

    Added positioning animation and shifting control panel (Main Window) to the side and video to top of it when call started

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { app, ipcMain } from 'electron'
+import { app, ipcMain, screen } from 'electron'
 import { MainWindowClass } from './windows/MainWindowClass'
 import { SideBarClass } from './windows/SideBarClass'
 
@@ -26,6 +26,10 @@ class Application {
 	handleEvents() {
 		ipcMain.on('open_sidebar', (_) => {
 			this.openSideBar()
+			// const { width, height } = screen.getPrimaryDisplay().workAreaSize
+			const [x, y] = this.AppContainer.InnerWindow.getPosition()
+			this.AppContainer.InnerWindow.setPosition(x + 100, y, true)
+			this.AppContainer.InnerWindow.setOpacity(0.5)
 		})
 		ipcMain.on('sidebar_open_check', (e) => {
 			e.returnValue = this.isSideBarOpen
@@ -44,7 +48,12 @@ class Application {
 			this.SideBarContainer = new SideBarClass()
 			this.SideBarContainer.init()
 
+			const [x, y] = this.AppContainer.InnerWindow.getPosition()
+			this.SideBarContainer.InnerWindow.setPosition(x - 200, y - 300, true)
+
 			this.SideBarContainer.InnerWindow.on('closed', () => {
+				this.AppContainer.InnerWindow.setPosition(x, y, true)
+				this.AppContainer.InnerWindow.setOpacity(1)
 				this.SideBarContainer = null
 				this.isSideBarOpen = false
 				if (this.SideBarContainer) {
