5e847992a5ac2b963593122cf6712732cec0790a
Saksham Puri <sakshampuri1999@gmail.com>
2021-01-25 21:19:17 +0530

    Removed unused passed state

--- app/components/Game/index.tsx
+++ app/components/Game/index.tsx
@@ -1,18 +1,16 @@
 import * as React from 'react'
-import { IAppState } from '../../App'
 
 import GameContainer from './GameContainer'
 
 import { draw } from './loader'
 
 interface GameProps {
-	gameState: IAppState
 	local: boolean
 }
 
-const Game: React.FC<GameProps> = ({ gameState, local }) => {
+const Game: React.FC<GameProps> = ({ local }) => {
 	//Extract state from gamestate as required to pass to draw
-	return <GameContainer {...{ draw }} local={local} />
+	return <GameContainer {...{ draw, local }} />
 }
 
 export default Game
