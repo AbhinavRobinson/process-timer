a088cb2e07ea3ded82d2608a88c6ffe767738fb1
Saksham Puri <sakshampuri1999@gmail.com>
2021-01-28 01:31:37 +0530

    Tracking browser title. TODO: Add settings to allow user to select what to track -> title, url, app

--- app/screens/App/game_loop.tsx
+++ app/screens/App/game_loop.tsx
@@ -1,17 +1,22 @@
 import { ipcRenderer } from 'electron'
 import { join } from 'path'
 import getFile from '../../../run'
-import activeWin from 'active-win'
 import { App } from './App'
+import activeWin from 'active-win'
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
-const ignoreApp = isDevelopment ? 'Electron' : 'Nudge'
 
-export function game_loop(obj: App) {
-	obj.active_app_interval_timeout = setInterval(() => {
+const ignoreApp = isDevelopment ? 'Electron' : 'Nudge'
+const executableName = process.platform === 'win32' ? 'getwindow.exe' : 'main'
+const executableLocation = join(__static, 'dist', executableName)
+
+const browsers = ['Safari', 'Chrome', 'Edge', 'Brave ']
+
+export async function game_loop(obj: App) {
+	obj.active_app_interval_timeout = setInterval(async () => {
 		// For Windows
 		if (process.platform === 'win32') {
-			getFile(join(__static, 'dist', 'getwindow.exe'), (data) => {
+			getFile(executableLocation, (data) => {
 				if (data['title'] !== ignoreApp) {
 					if (data['app'] === 'chrome.exe') {
 						if (data['title'] !== obj.state.active_app) obj.setState({ active_app: data['title'] })
@@ -20,28 +25,18 @@ export function game_loop(obj: App) {
 			})
 		} else {
 			// For MacOS and Linux
-			if (process.platform === 'darwin') {
-				;(async () => {
-					const data = await activeWin()
-					const appName: string = data?.owner?.name
-					if (!appName) return null
-					if (appName.toUpperCase() !== ignoreApp.toUpperCase() && obj.state.active_app.toUpperCase() !== data.owner?.name.toUpperCase())
-						obj.setState({ active_app: data.owner?.name.toUpperCase() })
-				})()
-			} else {
-				;(async () => {
-					const data = await activeWin()
-					const appName: string = data?.owner?.name
-					if (!appName) return null
-					if (appName.toUpperCase() !== ignoreApp.toUpperCase()) {
-						if (appName.toUpperCase() === 'GOOGLE-CHROME') {
-							obj.setState({ active_app: data.title.toUpperCase() })
-						} else if (obj.state.active_app.toUpperCase() !== data.owner?.name.toUpperCase())
-							obj.setState({ active_app: data.owner?.name.toUpperCase() })
-					}
-				})()
-			}
+
+			const data = await activeWin()
+			const appName: string = data?.owner?.name
+
+			if (!appName || appName.toUpperCase() === ignoreApp.toUpperCase()) return null
+
+			if (browsers.some((browser) => appName.toUpperCase().includes(browser.toUpperCase()))) {
+				if (obj.state.active_app.toUpperCase() !== data.title.toUpperCase()) obj.setState({ active_app: data.title.toUpperCase() })
+			} else if (obj.state.active_app.toUpperCase() !== data.owner?.name.toUpperCase())
+				obj.setState({ active_app: data.owner?.name.toUpperCase() })
 		}
+
 		ipcRenderer.emit('time_data', obj.state.time_spent)
 	}, 2000)
 }
--- run.ts
+++ run.ts
@@ -1,28 +1,45 @@
-// const { execFile } = require('child_process')
+import { spawn } from 'child_process'
 
-const { spawn } = require('child_process')
-// const { join } = require('path')
-
-// const filepath = join('C:\\Users\\Aniket\\nims\\static\\getwindow.py')
-
-// console.log(filepath)
+/*
+* Sample data returned by active-win binary 'main'
+	{
+		title: 'Unicorns - Google Search',
+		id: 5762,
+		bounds: {
+			x: 0,
+			y: 0,
+			height: 900,
+			width: 1440
+		},
+		owner: {
+			name: 'Google Chrome',
+			processId: 310,
+			bundleId: 'com.google.Chrome',
+			path: '/Applications/Google Chrome.app'
+		},
+		url: 'https://sindresorhus.com/unicorn',
+		memoryUsage: 11015432
+	}
+	*/
 
 interface JSONdata {
 	title: string
 	app: string
 	pid: number
+	id: number
+	owner?: {
+		name: string
+		processId: number
+		bundleId: string
+		path: string
+	}
+	url?: string
 }
 
-export default function getFile(filepath, callback: (json_data: JSONdata) => void) {
-	// const pyprocess = spawn('python', [filepath])
+export default function getFile(filepath: string, callback: (json_data: JSONdata) => void) {
 	const pyprocess = spawn(filepath, [])
 
 	pyprocess.stdout.on('data', function (data) {
 		callback(JSON.parse(data.toString()))
 	})
 }
-
-// getFile('C:\\Users\\Aniket\\nims\\static\\getwindow.py', (data) => {
-// 	console.log(data)
-// 	console.log(JSON.parse(data))
-// })
